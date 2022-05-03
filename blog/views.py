43ec06614236eb9eda1f3da2e13a42df310b24fe
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-05-03 21:48:01 +0200

    chore: fixed post publish bug

--- blog/views.py
+++ blog/views.py
@@ -79,7 +79,7 @@ def comment_approve(request, pk):
 
 @login_required
 def post_publish(request, pk):
-    post = get_object_or_404(Post, pk)
+    post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
 
